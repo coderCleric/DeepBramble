f12437c15d32a29ccee4c4fba95a4e4d9d52c021
coderCleric <wescamphouse@gmail.com>
2023-01-13 18:04:21 -0700

    The baby fish now latches onto the player on bite

--- NewHorizonsTemplate/BabyBiter.cs
+++ NewHorizonsTemplate/BabyBiter.cs
@@ -10,6 +10,16 @@ namespace DeepBramble
 {
     class BabyBiter : MonoBehaviour
     {
+        Transform fishTransform = null;
+
+        /**
+         * On start, grab the fish that we're attached to
+         */
+        public void Start()
+        {
+            this.fishTransform = this.transform.parent;
+        }
+
         /**
          * When something biteable enters the bit trigger, latch onto it
          */
@@ -19,11 +29,25 @@ namespace DeepBramble
             OWRigidbody otherBody = other.gameObject.GetAttachedOWRigidbody();
 
             //Only do stuff if it's the ship or the player
-            if(otherBody.CompareTag("Player") || otherBody.CompareTag("Ship"))
+            if(otherBody.CompareTag("Player"))
             {
                 DeepBramble.debugPrint("Either player or ship should be bitten");
 
-                //First, latch onto them and enter a trance
+                //Set the other object as our parent so we move with it
+                fishTransform.SetParent(other.transform, true);
+
+                //Destroy/disable just about everything related to the fish movement
+                Component.Destroy(fishTransform.GetComponent<ImpactSensor>());
+                Component.Destroy(fishTransform.GetComponent<BabyFishController>());
+                Component.Destroy(fishTransform.GetComponent<OWRigidbody>());
+                foreach (Collider i in fishTransform.GetComponentsInChildren<Collider>())
+                    i.enabled = false;
+                foreach (OWCollider i in fishTransform.GetComponentsInChildren<OWCollider>())
+                    i.enabled = false;
+
+                //Set the fish to idle and disable the brain
+                fishTransform.GetComponent<BabyFishController>().ChangeState(AnglerfishController.AnglerState.Lurking);
+                fishTransform.GetComponent<BabyFishController>().enabled = false;
 
                 //Second, if it's the player, start hurting them
             }
