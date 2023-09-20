9ab3796e883ae3bd2b9bc9148c248ed36dea7a03
coderCleric <wescamphouse@gmail.com>
2023-09-20 14:51:14 -0600

    Improved simplified colliders for the eye symbol and the Dree houses.

--- NewHorizonsTemplate/DeepBramble.cs
+++ NewHorizonsTemplate/DeepBramble.cs
@@ -559,10 +559,10 @@ namespace DeepBramble
             if (Keyboard.current[Key.N].wasPressedThisFrame)
             {
                 //Vector3 point = new Vector3(18.1f, -108.8f, 28770.3f); //Graviton's Folly
-                //Vector3 point = new Vector3(9968.0f, -7.1f, -158.7f); //Dree planet
+                Vector3 point = new Vector3(9968.0f, -7.1f, -158.7f); //Dree planet
                 //Vector3 point = new Vector3(9559.7f, 9920.6f, -99.4f); //Language Dimension
                 //Vector3 point = new Vector3(-24.7f, 10043.4f, -244.6f); //Lava planet start
-                Vector3 point = new Vector3(-257.3f, 9950.4f, 39.4f); //Quantum cave
+                //Vector3 point = new Vector3(-257.3f, 9950.4f, 39.4f); //Quantum cave
                 //Vector3 point = new Vector3(85.7f, -3.3f, -9960.4f); //Poison planet
                 Transform absCenter = null;
                 foreach (AstroObject i in Component.FindObjectsOfType<AstroObject>())
@@ -579,7 +579,7 @@ namespace DeepBramble
                 FogWarpDetector shipDetector = Locator.GetShipDetector().GetComponent<FogWarpDetector>();
                 if (shipDetector.GetOuterFogWarpVolume() != null) {
                     Patches.fogRepositionHandled = true;
-                    shipDetector.GetOuterFogWarpVolume().WarpDetector(shipDetector, GameObject.Find("HotDimension_Body/Sector/OuterWarp").GetComponent<OuterFogWarpVolume>());
+                    shipDetector.GetOuterFogWarpVolume().WarpDetector(shipDetector, GameObject.Find("DreeDimension_Body/Sector/OuterWarp").GetComponent<OuterFogWarpVolume>());
                 }
 
                 Locator._shipBody.SetPosition(point);
