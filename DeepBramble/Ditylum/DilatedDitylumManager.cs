df770879fc379961ea795f685ded252515086bc8
coderCleric <56094451+coderCleric@users.noreply.github.com>
2025-06-12 10:40:54 -0600

    More achievement stuff and work on custom endings

--- DeepBramble/Ditylum/DilatedDitylumManager.cs
+++ DeepBramble/Ditylum/DilatedDitylumManager.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using NewHorizons.Components;
+using NewHorizons.External.Modules;
+using UnityEngine;
 
 namespace DeepBramble.Ditylum
 {
     public class DilatedDitylumManager : MonoBehaviour
     {
+        public float supernovaTime = -1;
+
         /**
          * Makes Ditylum look at the player, matching their up direction
          */
@@ -11,6 +15,19 @@ namespace DeepBramble.Ditylum
         {
             transform.LookAt(Locator.GetPlayerTransform(), Locator.GetPlayerTransform().up);
             gameObject.GetComponent<Animator>().SetTrigger("turn");
+        }
+
+        /**
+         * If needed, count down to the supernova
+         */
+        private void Update()
+        {
+            if (supernovaTime > 0 && Time.time > supernovaTime)
+            {
+                supernovaTime = -1;
+                GameOverModule GOModule = new GameOverModule() {text = "YOU WERE BOTH CONSUMED BY A FOREIGN STAR"};
+                NHGameOverManager.Instance.StartGameOverSequence(GOModule, DeathType.Supernova, DeepBramble.instance);
+            }
         }
     }
 }
